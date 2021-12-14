8a88a6971381ee23ed4269c90867f397a0579177
Vu Hoang <hatakebee.konoha@gmail.com>
2021-12-14 12:22:25 +0700

    register

--- common/database.go
+++ common/database.go
@@ -11,6 +11,7 @@ import (
 )
 
 var DB *gorm.DB
+var TESTDB *gorm.DB
 
 // SetupDB : initializing mysql database
 func InitDB() {
@@ -31,4 +32,24 @@ func InitDB() {
 // Using this function to get a connection, you can create your connection pool here.
 func GetDB() *gorm.DB {
 	return DB
+}
+
+func InitTestDB() {
+	USER := os.Getenv("TESTDB_USER")
+	PASS := os.Getenv("TESTDB_PASS")
+	HOST := os.Getenv("TESTDB_HOST")
+	PORT := os.Getenv("TESTDB_PORT")
+	DBNAME := os.Getenv("TESTDB_NAME")
+	log.Info("TESTDB_NAME: ", DBNAME)
+	URL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", USER, PASS, HOST, PORT, DBNAME)
+	db, err := gorm.Open("mysql", URL)
+	if err != nil {
+		panic(err.Error())
+	}
+	TESTDB = db
+}
+
+// Using this function to get a connection, you can create your connection pool here.
+func GetTestDB() *gorm.DB {
+	return TESTDB
 }
